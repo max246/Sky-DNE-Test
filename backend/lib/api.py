aedd4d7ae23b069cee5175980e27cfb347ab6699
christian bianchini <max246@gmail.com>
2022-05-29 09:03:56 +0100

    add mock up router methods add 3 endpoints to manipulate loopback

--- backend/lib/api.py
+++ backend/lib/api.py
@@ -1,8 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request,abort,jsonify
 from lib.vsr1000 import *
 
 api = Blueprint('api', __name__)
+router = VSR1000("host", "port", "user", "pass")
 
-@api.route('/')
-def index():
-    return "hello"
+
+@api.route('/',methods=['GET'])
+def list_loopback():
+    router.list_loopback()
+    return jsonify({"result": "ok", "message": "list loopback"})
+
+@api.route('/loopback/add',methods=['POST'])
+def add_loopback():
+    data = request.get_json()
+    if "id" in data and "ip" in data:
+        id = data['id']
+        ip = data['ip']
+        router.add_loopback(id, ip)
+        return jsonify({"result": "ok", "message":"add loopback"})
+    else:
+        abort(500)
+
+@api.route('/loopback/del',methods=['POST'])
+def del_loopback():
+    data = request.get_json()
+    if "id" in data and "ip" in data:
+        id = data['id']
+        ip = data['ip']
+        router.del_loopback(id, ip)
+        return jsonify({"result":"ok", "message": "del loopback"})
+    else:
+        abort(500)
--- backend/lib/vsr1000.py
+++ backend/lib/vsr1000.py
@@ -2,5 +2,57 @@ from ncclient import manager
 
 class VSR1000:
 
-    def __init__(self):
+    def __init__(self, host, port, user, password):
         print("VSR1000")
+        self._host = host
+        self._port = port
+        self._user = user
+        self._pass = password
+
+    def edit_config(self, config):
+        with manager.connect(host = self._host,
+                            port = self._port,
+                            username = self._user,
+                            password = self._pass,
+                             hostkey_verify = False) as m:
+            result = m.edit_config(target='running', config=config)
+            return result
+        return None
+
+    def get_config(self, config):
+        with manager.connect(host=self._host,
+                             port=self._port,
+                             username=self._user,
+                             password=self._pass,
+                             hostkey_verify=False) as m:
+            result = m.get(('subtree', config))
+            return result
+        return None
+
+    def get_list_loopback(self):
+        return ["loop inet"]
+
+    def add_loopback(self, id, ip):
+        try:
+            config = '<>{}{}</>'.format(id, ip)
+            result = self.edit_config(config)
+            return ["good"]
+        except:
+            return None
+
+    def del_loopback(self, id, ip):
+        try:
+            config = '<>{}{}</>'.format(id, ip)
+            result = self.edit_config(config)
+            return ["good"]
+        except:
+            return None
+
+    def list_loopback(self):
+        try:
+            config = '<intf-items><lb-items/></intf-items>'
+            result = self.get_config(config)
+            return ["good"]
+        except:
+            return None
+
